6091a11f271c9b0d21e9644a05fef7095e2247d5
Muhammad Ihsan <mihsan191@gmail.com>
2017-09-03 20:32:26 +1000

    refactor: Move bookshelf changer to BookshelfChanger component

--- src/Book.js
+++ src/Book.js
@@ -1,34 +1,16 @@
 import React, { Component } from 'react';
-import * as Constants from './utils/Constants';
+import BookshelfChanger from './BookshelfChanger'
 
 class Book extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.props.onUpdateBookShelf(this.props.book, event.target.value);
-  }
-
   render() {
-    const { book } = this.props;
+    const { book, onUpdateBookShelf } = this.props;
 
     return (
       <li>
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-            <div className="book-shelf-changer">
-              <select value={book.shelf} onChange={this.handleChange}>
-                <option value={Constants.NONE} disabled>Move to...</option>
-                <option value={Constants.CURRENTLY_READING}>Currently Reading</option>
-                <option value={Constants.WANT_TO_READ}>Want to Read</option>
-                <option value={Constants.READ}>Read</option>
-                <option value={Constants.NONE}>None</option>
-              </select>
-            </div>
+            <BookshelfChanger book={book} onUpdateBookShelf={onUpdateBookShelf} />
           </div>
           <div className="book-title">{book.title}</div>
           <div className="book-authors">{book.authors.join(', ')}</div>
---
+++ src/BookshelfChanger.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import * as Constants from './utils/Constants';
+
+class BookshelfChanger extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.onUpdateBookShelf(this.props.book, event.target.value);
+  }
+
+  render() {
+    const { book } = this.props;
+
+    return (
+      <div className="book-shelf-changer">
+        <select value={book.shelf} onChange={this.handleChange}>
+          <option value={Constants.NONE} disabled>Move to...</option>
+          <option value={Constants.CURRENTLY_READING}>Currently Reading</option>
+          <option value={Constants.WANT_TO_READ}>Want to Read</option>
+          <option value={Constants.READ}>Read</option>
+          <option value={Constants.NONE}>None</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default BookshelfChanger;
