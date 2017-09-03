ac98ba39d94ed637cb376e261f64291a7ba54e49
Muhammad Ihsan <mihsan191@gmail.com>
2017-09-03 23:50:51 +1000

    refactor: Move search books bar to SearchBooksBar component

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,44 +1,24 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import * as BooksAPI from './utils/BooksAPI';
+import SearchBooksBar from './SearchBooksBar';
 import SearchBooksResults from './SearchBooksResults';
 
 class SearchBooks extends Component {
-  constructor(props) {
-    super (props);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
   static propTypes = {
     myBooks: PropTypes.array.isRequired,
     onUpdateBookshelf: PropTypes.func.isRequired
   }
 
   state = {
-    books: [],
-    searchValue: ''
+    books: []
   }
 
-  handleChange = (event) => {
-    this.setState({ searchValue: event.target.value }, this.searchBooks);
-  }
-
-  searchBooks = () => {
-    if (this.state.searchValue === '') {
-      this.setState({ books: [] });
-    }
-    else {
-      BooksAPI.search(this.state.searchValue, 20).then((result) => {
-        if (result && !result.error) {
-          this.setState({ books: result });
-        }
-      });
-    }
+  setBooks = (books) => {
+    this.setState({ books: books });
   }
 
   render = () => {
-    const { books, searchValue } = this.state;
+    const { books } = this.state;
     const { myBooks, onUpdateBookshelf } = this.props;
 
     myBooks.forEach((myBook) => {
@@ -50,12 +30,7 @@ class SearchBooks extends Component {
 
     return (
       <div className="search-books">
-        <div className="search-books-bar">
-          <Link className='close-search' to='/'>Close</Link>
-          <div className="search-books-input-wrapper">
-            <input type="text" value={searchValue} onChange={this.handleChange} placeholder="Search by title or author" />
-          </div>
-        </div>
+        <SearchBooksBar onSearchBooks={this.setBooks} />
         <SearchBooksResults books={books} onUpdateBookshelf={onUpdateBookshelf} />
       </div>
     );
---
+++ src/SearchBooksBar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import * as BooksAPI from './utils/BooksAPI';
+
+class SearchBooksBar extends Component {
+  constructor(props) {
+    super (props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  static propTypes = {
+    onSearchBooks: PropTypes.func.isRequired
+  }
+
+  state = {
+    searchValue: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({ searchValue: event.target.value }, this.searchBooks);
+  }
+
+  searchBooks = () => {
+    if (this.state.searchValue === '') {
+      this.props.onSearchBooks([])
+    }
+    else {
+      BooksAPI.search(this.state.searchValue, 20).then((result) => {
+        if (result && !result.error) {
+          this.props.onSearchBooks(result);
+        }
+      });
+    }
+  }
+
+  render() {
+    const { searchValue } = this.state;
+
+    return (
+      <div className="search-books-bar">
+        <Link className='close-search' to='/'>Close</Link>
+        <div className="search-books-input-wrapper">
+          <input type="text" value={searchValue} onChange={this.handleChange} placeholder="Search by title or author" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchBooksBar;
