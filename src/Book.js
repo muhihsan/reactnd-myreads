d68dbad248e3967ba27d65b27be7a1c2fb44a00f
Muhammad Ihsan <mihsan191@gmail.com>
2017-09-03 16:02:43 +1000

    feat: Assign initial shelf value to book

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import * as Constants from './utils/Constants';
 
 class Book extends Component {
+  state = {
+    shelf: this.props.book.shelf
+  }
+
   render() {
+    const { shelf } = this.state;
     const { book } = this.props;
 
     return (
@@ -10,12 +16,12 @@ class Book extends Component {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
             <div className="book-shelf-changer">
-              <select>
-                <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
+              <select value={shelf}>
+                <option value={Constants.NONE} disabled>Move to...</option>
+                <option value={Constants.CURRENTLY_READING}>Currently Reading</option>
+                <option value={Constants.WANT_TO_READ}>Want to Read</option>
+                <option value={Constants.READ}>Read</option>
+                <option value={Constants.NONE}>None</option>
               </select>
             </div>
           </div>
