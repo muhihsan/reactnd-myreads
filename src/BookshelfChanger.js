3d03aef690e83c92d3cc99af20a50d9120ca9ca2
Muhammad Ihsan <mihsan191@gmail.com>
2017-09-04 22:07:31 +1000

    feat: Show loading icon when updating a book's shelf

--- src/BookshelfChanger.js
+++ src/BookshelfChanger.js
@@ -8,28 +8,41 @@ class BookshelfChanger extends Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
+  state = {
+    isChangingShelf: false
+  }
+
   static propTypes = {
     book: PropTypes.object.isRequired,
     onUpdateBookshelf: PropTypes.func.isRequired
   }
 
+  componentWillReceiveProps = (nextProps) => {
+    debugger;
+    this.setState({ isChangingShelf: nextProps.book.shelf && (nextProps.book.shelf !== this.props.book.shelf) });
+  }
+
   handleChange = (event) => {
+    this.setState({ isChangingShelf: true });
     this.props.onUpdateBookshelf(this.props.book, event.target.value);
   }
 
   render = () => {
+    const { isChangingShelf } = this.state;
     const { book } = this.props;
     const shelf = book.shelf ? book.shelf : Constants.NONE;
 
     return (
-      <div className="book-shelf-changer">
-        <select value={shelf} onChange={this.handleChange}>
-          <option disabled>Move to...</option>
-          <option value={Constants.CURRENTLY_READING}>Currently Reading</option>
-          <option value={Constants.WANT_TO_READ}>Want to Read</option>
-          <option value={Constants.READ}>Read</option>
-          <option value={Constants.NONE}>None</option>
-        </select>
+      <div className={`book-shelf-changer ${isChangingShelf ? 'loading' : 'changer'}`}>
+        {!isChangingShelf && (
+          <select value={shelf} onChange={this.handleChange}>
+            <option disabled>Move to...</option>
+            <option value={Constants.CURRENTLY_READING}>Currently Reading</option>
+            <option value={Constants.WANT_TO_READ}>Want to Read</option>
+            <option value={Constants.READ}>Read</option>
+            <option value={Constants.NONE}>None</option>
+          </select>
+        )}
       </div>
     );
   }
