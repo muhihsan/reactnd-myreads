67d56d936e3156496b3305f364652382a2caf6c9
Muhammad Ihsan <mihsan191@gmail.com>
2017-09-07 16:47:26 +1000

    feat: Search books on page load based on the query string value

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import * as QueryString from 'query-string';
 import * as Constants from './utils/Constants';
 import * as BooksAPI from './utils/BooksAPI';
 import SearchBooksBar from './SearchBooksBar';
@@ -24,41 +25,72 @@ class SearchBooks extends Component {
   }
 
   /**
+   * After BooksApp component has been mounted.
+   * Do initial search.
+   */
+  componentDidMount = () => {
+    this.initialSearch();
+  }
+
+  /**
+   * Get search value from query string.
+   * Search for books if the correct query string is found.
+   * Set the searchValue state.
+   */
+  initialSearch = () => {
+    const search = this.props.history.location.search;
+    if (search) {
+      const queryString = QueryString.parse(search);
+      const searchValue = queryString.q? queryString.q : '';
+      this.searchBooks(searchValue);
+      this.setState({ searchValue: searchValue });
+    }
+  }
+
+  /**
    * Change the current search value state.
    * Search the book only when after certain timeout period.
    * @param {object} event - This event value.
    * @param {object} event.target - This target value.
-   * @param {string} event.target.value - This value vale.
+   * @param {string} event.target.value - This value value.
    */
   handleTextChange = (event) => {
     if (this.state.typingTimeOut)
       clearTimeout(this.state.typingTimeOut);
 
+    const value = event.target.value;
     this.setState({
-      searchValue: event.target.value,
-      typingTimeOut: setTimeout(() => this.searchBooks(), 1500)
+      searchValue: value,
+      typingTimeOut: setTimeout(() => this.updateQueryStringThenSearchBooks(value), 1500)
     });
   }
 
   /**
-   * Update search value query string.
    * Set the current search results to empty books when the search value is empty.
    * Find books through BooksAPI if the current search value is not empty.
    * Set the current search results to the result of the BooksAPI call.
+   * @param {object} searchValue - This searchValue value.
    */
-  searchBooks = () => {
-    this.updateSearchQueryString();
-
-    if (this.state.searchValue.trim() === '') {
-      this.setBooksAndSearchValue([])
+  searchBooks = (searchValue) => {
+    if (searchValue.trim() === '') {
+      this.setBooksAndCurrentSearchValue([])
     }
     else {
       this.setState({ isSearchingBooks: true });
-      BooksAPI.search(this.state.searchValue, 20).then((result) => {
-        this.setBooksAndSearchValue((result && !result.error) ? result : [], this.state.searchValue);
+      BooksAPI.search(searchValue, 20).then((result) => {
+        this.setBooksAndCurrentSearchValue((result && !result.error) ? result : [], searchValue);
         this.setState({ isSearchingBooks: false });
       });
     }
+  }
+
+  /**
+   * Update search value query string.
+   * Call search books.
+   */
+  updateQueryStringThenSearchBooks = (searchValue) => {
+    this.updateSearchQueryString();
+    this.searchBooks(searchValue);
   }
 
   /**
@@ -66,7 +98,7 @@ class SearchBooks extends Component {
    * @param {Object[]} searchedBooks - The searchBooks value.
    * @param {string} currentSearchValue - The currentSearchValue value.
    */
-  setBooksAndSearchValue = (searchedBooks, currentSearchValue = '') => {
+  setBooksAndCurrentSearchValue = (searchedBooks, currentSearchValue = '') => {
     this.setState({
       searchedBooks: searchedBooks,
       currentSearchValue: currentSearchValue
